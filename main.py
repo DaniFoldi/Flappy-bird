6b46cd1ebe4fc13867a2db39f4cb6cea07e8b0f0
DaniFoldi <dani@stefan.hu>
2017-07-16 22:21:49 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import turtle
+
+
+#TURTLES
+backgorund_turtle = turtle.Turtle()
+
+character_turtle = turtle.Turtle()
+
+obstacle1_turtle = turtle.Turtle()
+obstacle2_turtle = turtle.Turtle()
+obstacle3_turtle = turtle.Turtle()
+
+game_window = turtle.window()
+
+
+
+#FUNCTIONS:
+def draw_background(selectedTurtle, height, witdth):
+
+def lose_game():
+
+def generate_obstacle():
+
+def character_jump():
+
+def character_gravity():
+
+def frame():
+
+def obstacle_shift():
+
+#MAIN LOGIC
+if __name__ == "__main__":
